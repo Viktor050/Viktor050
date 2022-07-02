b8f4f0008d46be7202b3bc148841bb4647ebac05
Viktor050 <102862057+Viktor050@users.noreply.github.com>
2022-07-02 16:32:22 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,18 +34,62 @@ if (isMobile.any) {
 		menuBurger.classList.toggle('_active');
 	});
 }
+// ==================================================
+
+const parentOriginal = document.querySelector('.parent-original');
+const parent = document.querySelector('.header-main-page-body');
+const item = document.querySelector('.header-main-page-content__img');
+
+window.addEventListener('resize', function (event) {
+	const viewPort_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	if (viewPort_width <= 767) {
+		if (!item.classList.contains('done')) {
+			parent.insertBefore(item, parent.children[4]);
+			item.classList.add('done');
+		}
+	} else {
+		if (item.classList.contains('done')) {
+			parentOriginal.insertBefore(item, parentOriginal.children[4]);
+			item.classList.remove('done');
+		}
+	}
+});
 
 
 
 
+
+
+// =================================================
 const swiper = new Swiper('.swiper', {
 	// navigation: {
 	// 	prevEl: '.swiper-button-prev',
 	// 	nextEl: '.swiper-button-next',
 	// },
+	// autoplay: {
+		// delay: 5000,
+		// disableOnInteraction: false,
+
+		// pauseOnMouseEnter: true, 
+		/*пауза при наведенні курсора*/
+	// },
 	pagination: {
 		el: '.swiper-pagination',
+		type: 'bullets',
 		clickable: true,
 		dynamicBullets: true,
 	},
+	hashNavigation: {
+		watchState: true,
+	},
+	keyboard: {
+		enabled: true,
+		onlyInViewport: true,
+		pageUpDown: true,
+	},
+	// mousewheel: {
+	// 	sensitivity: 1,
+	// }, 
+	/*прокрутка мишкою(тачскріном). Тачскріном не можна проскролити сторінку бо блок займає всю ширину*/
+	autoHeight: true,
 });
